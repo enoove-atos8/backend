e941d4467782b0984d63e04b7249a3292204fe67
Rafael Henrique Melo de Souza <rhms@dmz.org.br>
2023-05-23 12:41:32 -0300

    Adjusts in central domains

--- routes/tenant.php
+++ routes/tenant.php
@@ -33,7 +33,7 @@ Route::prefix('api')->middleware(['api', InitializeTenancyBySubdomain::class, Pr
 
     Route::get('/version', function () {
         return [
-            'api_version'   =>  '00.00.023',
+            'api_version'   =>  '00.00.026',
             'branch'        =>  'develop',
             'tenant'        =>  tenant('id')
         ];
