de7813e9530ebe9929b38dca2d0140ab960f8469
Rafael Henrique Melo de Souza <rhms@dmz.org.br>
2023-05-22 23:29:37 -0300

    Adjusts in central domains

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,6 +17,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
+        'www.atos242.com',
         'atos242.com',
     ],
 
