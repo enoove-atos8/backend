cf7533fb377c6284e1125e9afa119b894bf5ac31
Rafael Henrique Melo de Souza <rhms@dmz.org.br>
2023-05-23 12:41:32 -0300

    Adjusts in central domains

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/version', function () {
     return [
-        'api_version'   =>  '00.00.023',
+        'api_version'   =>  '00.00.026',
         'branch'        =>  'develop',
         'tenant'        =>  'central domain'
     ];
