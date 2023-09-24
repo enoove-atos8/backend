4a2fe5fc6b76be0c7ba81c62b8927faf6b5a8513
Rafael Henrique Melo de Souza <rhms@dmz.org.br>
2023-09-24 17:50:04 -0300

    AT242-54 - Create table user_details and delete some files

---
+++ database/migrations/tenant/2023_09_24_000000_create_user_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->integer('user_id')->nullable(false);
+            $table->string('full_name')->nullable(false);
+            $table->string('avatar')->nullable();
+            $table->string('type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_details');
+    }
+};
