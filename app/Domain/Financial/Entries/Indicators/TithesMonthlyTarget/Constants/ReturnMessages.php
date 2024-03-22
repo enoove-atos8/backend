100a227a37cfb3f2edb6f2294b79de660dfe6fb3
Rafael Souza <rhms@dmz.org.br>
2024-03-22 09:48:26 -0300

    Implements Tithes Monthly Target and some adjusts

--- app/Domain/Financial/Entries/Indicators/TithesMonthlyTarget/Constants/ReturnMessages.php
+++ app/Domain/Financial/Entries/Indicators/TithesMonthlyTarget/Constants/ReturnMessages.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Domain\Financial\Entries\Indicators\MonthlyTarget\Constants;
+namespace Domain\Financial\Entries\Indicators\TithesMonthlyTarget\Constants;
 
 class ReturnMessages
 {
