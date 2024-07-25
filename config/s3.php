c5062f5d595f52d343f081385d04d4063b8d31f8
Rafael Souza <rhms@dmz.org.br>
2024-07-25 15:00:34 -0300

    Adjusts in s3 config and other things

--- config/s3.php
+++ config/s3.php
@@ -15,12 +15,12 @@ return [
 
         ],
         'local'   =>  [
-            'S3_BASE_URL'                   =>  'http://localhost:9001/api/v1/buckets/',
+            'S3_BASE_URL'                   =>  'http://s3.atos8.local:9001/api/v1/buckets/',
             'S3_ACCESS_KEY_ID'              =>  'rhms',
             'S3_SECRET_ACCESS_KEY'          =>  'Souza.9559',
             'S3_DEFAULT_REGION'             =>  'us-east-1',
             'S3_ENDPOINT'                   =>  'http://minio:9000',
-            'S3_ENDPOINT_EXTERNAL_ACCESS'   =>  'http://localhost:9090/',
+            'S3_ENDPOINT_EXTERNAL_ACCESS'   =>  'http://s3.atos8.local:9090',
             'S3_USE_PATH_STYLE_ENDPOINT'    =>  true,
         ],
     ],
