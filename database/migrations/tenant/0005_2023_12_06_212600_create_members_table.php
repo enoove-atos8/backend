3a7c8095033ae7e8032211a2eda1c7fb9e4f6a94
Rafael Souza <rhms@dmz.org.br>
2024-08-10 21:13:30 -0300

    Tabelas e Seeders de Divisões eclesiásticas e areas

--- database/migrations/tenant/0005_2023_12_06_212600_create_members_table.php
+++ database/migrations/tenant/0005_2023_12_06_212600_create_members_table.php
@@ -14,16 +14,25 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('members', function (Blueprint $table) {
-
             $table->integer('id', true)->autoIncrement();
-            $table->integer('activated')->default('0');
-            $table->integer('deleted')->default('0');
+
+            // Relationships with ecclesiastical divisions (ministry, department, organization, event)
+            $table->integer('ecclesiastical_divisions_area_id')->nullable();
+            $table->boolean('leader')->nullable()->default(0);
+
+            // Status of the member
+            $table->boolean('activated')->default(0);
+            $table->boolean('deleted')->default(0);
+
+            // Profile information
             $table->string('avatar')->nullable();
             $table->string('full_name')->nullable(false);
             $table->string('gender')->nullable(false);
             $table->string('cpf')->nullable()->unique();
             $table->string('rg')->nullable()->unique();
             $table->string('work')->nullable();
+
+            // Personal and contact details
             $table->string('born_date')->nullable(false);
             $table->string('email')->nullable();
             $table->string('phone')->nullable();
@@ -32,21 +41,33 @@ return new class extends Migration
             $table->string('district')->nullable(false);
             $table->string('city')->nullable(false);
             $table->string('uf')->nullable(false);
+
+            // Family and additional information
             $table->string('marital_status')->nullable();
             $table->string('spouse')->nullable();
             $table->string('father')->nullable();
             $table->string('mother')->nullable(false);
             $table->string('ecclesiastical_function')->nullable();
+
+            // Member type and additional information
             $table->string('member_type')->nullable(false);
-            $table->string('ministries')->nullable();
             $table->string('baptism_date')->nullable();
             $table->string('blood_type')->nullable();
             $table->string('education')->nullable();
 
-
+            // Remember token for authentication
             $table->rememberToken();
+
+            // Date and time control fields
             $table->timestamps();
+
+            // Relationships with ecclesiastical divisions areas
+            $table->foreign('ecclesiastical_divisions_area_id')
+                ->references('id')
+                ->on('ecclesiastical_divisions_areas')
+                ->onDelete('set null');
         });
+
     }
 
     /**
