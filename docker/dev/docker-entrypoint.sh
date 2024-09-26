b7888be08013ac7f4040c06e2b9b51ae00aeaa36
Rafael Souza <rhms@dmz.org.br>
2024-09-26 10:38:48 -0300

    Comentando start octane

--- docker/dev/docker-entrypoint.sh
+++ docker/dev/docker-entrypoint.sh
@@ -5,4 +5,4 @@ cron -f &
 
 # Start Octane server
 echo "Starting Octane on port: 8000"
-php artisan octane:start --server=swoole --host=0.0.0.0 --port=8000
+#php artisan octane:start --server=swoole --host=0.0.0.0 --port=8000
