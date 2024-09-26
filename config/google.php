d0359d9faecf1d1797d87294b1c59e7e21b4bc2f
Rafael Souza <rhms@dmz.org.br>
2024-09-26 17:09:39 -0300

    Ajustando o tenant para stage no config/google.php

--- config/google.php
+++ config/google.php
@@ -3,7 +3,7 @@
 return [
     'drive' =>  [
         'tenants'    =>  [
-            'iebrd' =>  [
+            'stage' =>  [
                 'GOOGLE_DRIVE_CLIENT_ID'    =>  '961839464859-9m0r05q7efvdh3t2kegvckrj8br7cngc.apps.googleusercontent.com',
                 'GOOGLE_DRIVE_CLIENT_SECRET'    =>  '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',
             ],
