8fdc0ceadb9aaf32bd40dc67757dd8aca89f514f
Rafael Souza <rhms@dmz.org.br>
2024-09-26 21:00:05 -0300

    Alterando a periodicidade do Job para everyTenMinutes

--- app/Application/Core/Console/Kernel.php
+++ app/Application/Core/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(function () {
             resolve(ProcessingEntriesByBankTransfer::class)->handle();
-        })->hourly();
+        })->everyTenMinutes();
     }
 
 
