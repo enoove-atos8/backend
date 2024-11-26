a234be264fed693f1882857eb295ff153953c259
Rafael Souza <rhms@dmz.org.br>
2024-11-26 14:19:54 -0300

    AT164 - Backend - Ajustes em migration e repositório createCult

--- database/migrations/tenant/2024_11_20_110600_create_cults_table.php
+++ database/migrations/tenant/2024_11_20_110600_create_cults_table.php
@@ -24,9 +24,9 @@ return new class extends Migration
                 $table->string('cult_date')->nullable(false);
                 $table->string('date_transaction_compensation')->nullable(false);
                 $table->string('transaction_type')->nullable(false);
-                $table->decimal('tithes_amount')->nullable();
-                $table->decimal('designated_amount')->nullable();
-                $table->decimal('offers_amount')->nullable();
+                $table->decimal('tithes_amount')->nullable()->default(0);
+                $table->decimal('designated_amount')->nullable()->default(0);
+                $table->decimal('offers_amount')->nullable()->default(0);
                 $table->boolean('deleted')->nullable(false)->default(0);
                 $table->string('receipt')->nullable(false);
                 $table->string('comments')->nullable();
