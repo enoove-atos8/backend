8845ab7597a685e47e2a80464911834073dc958f
Rafael Souza <rhms@dmz.org.br>
2024-12-03 15:17:14 -0300

    AT8-186 - Backend - Ajustes na conversão da data do $this->consolidationEntriesData->date

--- app/Application/Core/Console/Kernel.php
+++ app/Application/Core/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(function () {
             resolve(ProcessingEntriesByBankTransfer::class)->handle();
-        })->dailyAt('12:01');
+        })->hourly();
 
         /*$schedule->call(function () {
             resolve(ProcessingEntriesByCollectionWorship::class)->handle();
