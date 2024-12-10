8daf58ccd1c6037e8843d8c28a9315848df53dc1
Rafael Souza <rhms@dmz.org.br>
2024-12-10 10:15:08 -0300

    Backend - Ajustando o regex do nubank

--- app/Infrastructure/Services/OCRExtractDataBankReceipt/ReceiptModelByInstitution.php
+++ app/Infrastructure/Services/OCRExtractDataBankReceipt/ReceiptModelByInstitution.php
@@ -115,10 +115,9 @@ class ReceiptModelByInstitution
             self::BANK_NUBANK => [
                 'amount' => ['/R\$.(\d.*)/'],
                 'name' => ['/Origem\s+Nome\s+([^\n]+)/'],
-                'date' => ['/\\d{2} [A-Z]{3} \\d{4}\\b/'],
+                'date' => ['/\d{2} [A-Z]{3} \d{4}/'],
                 'cpf' => [
-                    '/CPF.*?(\d{3})[,.]*\d?[,.]*([\d][ ]?\d[ ]?\d)/',
-                    '/.*?(\d{3})[,](\d{3})/'
+                    '/CPF.*?(\d{3}\.\d{3})/',
                 ],
                 'timestamp' => '/(\d{2} \w{3} \d{4}) - (\d{2}:\d{2}:\d{2})/'
             ],
