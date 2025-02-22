354bdf7fb055f1b0139b692ff37af1fb7481f217
Rafael Souza <rhms@dmz.org.br>
2025-02-22 11:15:50 -0300

    Backend - Ajustando retorno das divisões para o front end e removendo a necessidade de criação de diretórios após criar divisões ou grupos

--- app/Domain/Ecclesiastical/Divisions/Actions/CreateNewDivisionAction.php
+++ app/Domain/Ecclesiastical/Divisions/Actions/CreateNewDivisionAction.php
@@ -6,36 +6,20 @@ use Domain\Ecclesiastical\Divisions\DataTransferObjects\DivisionData;
 use Domain\Ecclesiastical\Divisions\Interfaces\DivisionRepositoryInterface;
 use Domain\Ecclesiastical\Divisions\Models\Division;
 use Domain\Ecclesiastical\Divisions\Constants\ReturnMessages;
-use Domain\Financial\SyncStorage\Actions\AddPathSyncStorageAction;
-use Domain\Financial\SyncStorage\Actions\GetSyncStorageDataByPathAction;
-use Domain\Financial\SyncStorage\DataTransferObjects\SyncStorageData;
 use Infrastructure\Exceptions\GeneralExceptions;
 use Infrastructure\Repositories\Ecclesiastical\Divisions\DivisionRepository;
-use Infrastructure\Util\Storage\S3\CreateDirectory;
 use Throwable;
 
 class CreateNewDivisionAction
 {
     private DivisionRepository $divisionRepository;
 
-    private AddPathSyncStorageAction $addPathSyncStorageAction;
-    private CreateDirectory $createDirectory;
-
-    private GetSyncStorageDataByPathAction $getSyncStorageDataByPathAction;
-
-    const BASE_PATH_DESIGNATED_ENTRIES_SHARED_RECEIPTS = 'sync_storage/financial/entries/shared_receipts/designated';
 
     public function __construct(
         DivisionRepositoryInterface $divisionRepository,
-        CreateDirectory $createDirectory,
-        AddPathSyncStorageAction $addPathSyncStorageAction,
-        GetSyncStorageDataByPathAction $getSyncStorageDataByPathAction
     )
     {
         $this->divisionRepository = $divisionRepository;
-        $this->createDirectory = $createDirectory;
-        $this->addPathSyncStorageAction = $addPathSyncStorageAction;
-        $this->getSyncStorageDataByPathAction = $getSyncStorageDataByPathAction;
     }
 
 
@@ -50,7 +34,6 @@ class CreateNewDivisionAction
         if(is_null($existDivision))
         {
             $division = $this->divisionRepository->createDivision($divisionData);
-            $this->createDirectory->createDirectory(self::BASE_PATH_DESIGNATED_ENTRIES_SHARED_RECEIPTS . '/'. $divisionData->slug, $tenant);
 
             if(!is_null($division->id))
             {
