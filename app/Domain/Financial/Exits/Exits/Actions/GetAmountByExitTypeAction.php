72c676b1cf2096c3c3133fb86920d912b73cd1d0
Rafael Souza <rhms@dmz.org.br>
2025-03-18 08:20:13 -0300

    Backend - Ajustes

---
+++ app/Domain/Financial/Exits/Exits/Actions/GetAmountByExitTypeAction.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Domain\Financial\Exits\Exits\Actions;
+
+use Domain\Financial\Exits\Exits\Interfaces\ExitRepositoryInterface;
+use Infrastructure\Repositories\BaseRepository;
+use Infrastructure\Repositories\Financial\Exits\Exits\ExitRepository;
+use Throwable;
+
+class GetAmountByExitTypeAction
+{
+    private ExitRepository $exitRepository;
+
+    public function __construct(ExitRepositoryInterface $exitRepositoryInterface)
+    {
+        $this->exitRepository = $exitRepositoryInterface;
+    }
+
+
+
+    /**
+     * @throws Throwable
+     */
+    public function execute($rangeDates, $exitType = 'all'): null | array
+    {
+        $exits = $this->exitRepository->getAmountByExitType($rangeDates, $exitType);
+        $totalPayment = $exits
+            ->where(ExitRepository::EXIT_TYPE_COLUMN,
+                BaseRepository::OPERATORS['EQUALS'],
+                ExitRepository::PAYMENT_VALUE)->sum(ExitRepository::AMOUNT_COLUMN);
+
+        $totalTransfer = $exits
+            ->where(ExitRepository::EXIT_TYPE_COLUMN,
+                BaseRepository::OPERATORS['EQUALS'],
+                ExitRepository::TRANSFER_VALUE)->sum(ExitRepository::AMOUNT_COLUMN);
+
+        $totalMinisterialTransfers = $exits
+            ->where(ExitRepository::EXIT_TYPE_COLUMN,
+                BaseRepository::OPERATORS['EQUALS'],
+                ExitRepository::MINISTERIAL_TRANSFER_VALUE)->sum(ExitRepository::AMOUNT_COLUMN);
+
+        $totalContributions = $exits
+            ->where(ExitRepository::CONTRIBUTIONS_VALUE,
+                BaseRepository::OPERATORS['EQUALS'],
+                ExitRepository::CONTRIBUTIONS_VALUE)->sum(ExitRepository::AMOUNT_COLUMN);
+
+
+        return [
+            'payments'              =>  $totalPayment,
+            'transfers'             =>  $totalTransfer,
+            'ministerialTransfers'  =>  $totalMinisterialTransfers,
+            'contributions'         =>  $totalContributions,
+        ];
+    }
+}
--- app/Domain/Financial/Exits/Exits/Interfaces/ExitRepositoryInterface.php
+++ app/Domain/Financial/Exits/Exits/Interfaces/ExitRepositoryInterface.php
@@ -9,4 +9,6 @@ use Infrastructure\Repositories\Financial\Exits\Exits\ExitRepository;
 interface ExitRepositoryInterface
 {
     public function getExits(?string $dates, array $filters, string $transactionCompensation = ExitRepository::COMPENSATED_VALUE, bool $paginate = true): Collection | Paginator;
+
+    public function getAmountByExitType(string $dates, string $exitType = '*'): mixed;
 }
