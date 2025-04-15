c5a984b6dd54f3639fbdb7d8e74b5b3b3d1c17c9
Rafael Souza <rhms@dmz.org.br>
2025-04-15 14:32:21 -0300

    Backend - Ajustes

--- app/Infrastructure/Repositories/Financial/Exits/Payments/PaymentItemRepository.php
+++ app/Infrastructure/Repositories/Financial/Exits/Payments/PaymentItemRepository.php
@@ -6,7 +6,9 @@ use App\Domain\Financial\Exits\Payments\Items\DataTransferObjects\PaymentItemDat
 use Domain\Financial\Exits\Payments\Items\Interfaces\PaymentItemRepositoryInterface;
 use Domain\Financial\Exits\Payments\Items\Models\PaymentItem;
 use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Infrastructure\Repositories\BaseRepository;
 
 class PaymentItemRepository extends BaseRepository implements PaymentItemRepositoryInterface
@@ -17,6 +19,13 @@ class PaymentItemRepository extends BaseRepository implements PaymentItemReposit
     const ID_COLUMN_JOINED = 'payment_item.id';
     const PAYMENT_CATEGORY_ID_COLUMN_JOINED = 'payment_item.payment_category_id';
     const TABLE_ALIAS = 'payment_item';
+    const PAGINATE_NUMBER = 999;
+
+    /**
+     * Array of conditions
+     */
+    private array $queryConditions = [];
+
 
     const DISPLAY_SELECT_COLUMNS = [
         'payment_item.id as payment_item_id',
@@ -37,7 +46,7 @@ class PaymentItemRepository extends BaseRepository implements PaymentItemReposit
         $this->queryConditions = [];
         $this->queryConditions [] = $this->whereEqual(self::PAYMENT_CATEGORY_ID_COLUMN_JOINED, $id, 'and');
 
-        return $this->getItemsWithRelationshipsAndWheres($this->queryConditions);
+        return $this->qbGetPaymentItems($this->queryConditions, self::DISPLAY_SELECT_COLUMNS, (array)self::ID_COLUMN_JOINED, false);
     }
 
 
@@ -61,5 +70,53 @@ class PaymentItemRepository extends BaseRepository implements PaymentItemReposit
             'name'                    =>   $paymentItemData->name,
             'description'             =>   $paymentItemData->description,
         ]);
+    }
+
+
+
+    /**
+     * Get entries with members and reviewers joins
+     *
+     * @param array $queryClausesAndConditions
+     * @param array $selectColumns
+     * @param array $orderBy
+     * @param bool $paginate
+     * @param string $sort
+     * @return Collection | Paginator
+     * @throws BindingResolutionException
+     */
+    public function qbGetPaymentItems(
+        array $queryClausesAndConditions,
+        array $selectColumns,
+        array $orderBy,
+        bool $paginate = true,
+        string $sort = 'desc'): Collection | Paginator
+    {
+        $query = function () use (
+            $queryClausesAndConditions,
+            $selectColumns,
+            $orderBy,
+            $sort,
+            $paginate) {
+
+            $q = DB::table(PaymentItemRepository::TABLE_NAME)
+                ->select($selectColumns)
+                ->where(function ($q) use($queryClausesAndConditions){
+                    $q->where($queryClausesAndConditions[0]['condition']['field'], $queryClausesAndConditions[0]['condition']['operator'], $queryClausesAndConditions[0]['condition']['value']);
+                });
+
+            if($paginate)
+            {
+                $paginator = $q->simplePaginate(self::PAGINATE_NUMBER);
+                return $paginator->setCollection($paginator->getCollection()->map(fn($item) => PaymentItemData::fromArray((array) $item)));
+            }
+            else
+            {
+                $results = $q->get();
+                return $results->map(fn($item) => PaymentItemData::fromArray((array) $item));
+            }
+        };
+
+        return $this->doQuery($query);
     }
 }
