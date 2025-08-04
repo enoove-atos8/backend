9ef066a5407a52516f20f281709540ab7e70df50
Rafael Souza <rhms@dmz.org.br>
2025-08-04 15:08:39 -0300

    Backend - Ajustes

--- app/Infrastructure/Repositories/Member/MemberRepository.php
+++ app/Infrastructure/Repositories/Member/MemberRepository.php
@@ -205,6 +205,18 @@ class MemberRepository extends BaseRepository implements MemberRepositoryInterfa
                             }
                         }
 
+                        if(Arr::exists($filters, 'ageFrom'))
+                        {
+                            $ageFrom = $filters['ageFrom'];
+                            $q->where(DB::raw("STR_TO_DATE(" . self::BORN_DATE_COLUMN . ", '%d%m%Y')"), '<=', now()->subYears($ageFrom)->toDateString());
+                        }
+
+                        if(Arr::exists($filters, 'ageTo'))
+                        {
+                            $ageTo = $filters['ageTo'];
+                            $q->where(DB::raw("STR_TO_DATE(" . self::BORN_DATE_COLUMN . ", '%d%m%Y')"), '>=', now()->subYears($ageTo)->toDateString());
+                        }
+
                     }
                 })
                 ->orderBy(self::FULL_NAME_COLUMN);
